0e91c3fb90578e91e4a5ea411c9f825d36b54618
Danny Guo <dannyguo91@gmail.com>
2020-09-14 22:55:34 -0400

    Add the color palette generator

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as iq from "image-q";
+
 function padTime(number) {
   return number < 10 ? "0" + number : number;
 }
@@ -18,3 +20,42 @@ function getTime() {
 setInterval(function () {
   document.getElementById("time").innerText = getTime();
 }, 50);
+
+function pointToRGB(point) {
+  return "rgb(" + point.r + "," + point.g + "," + point.b + ")";
+}
+
+document
+  .getElementById("image-url-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const url = event.target.elements.url.value;
+    const image = document.getElementById("image");
+    image.onload = function () {
+      const canvas = document.createElement("canvas");
+      canvas.width = image.naturalWidth;
+      canvas.height = image.naturalHeight;
+      const context = canvas.getContext("2d");
+      context.drawImage(image, 0, 0);
+      const imageData = context.getImageData(
+        0,
+        0,
+        image.naturalWidth,
+        image.naturalHeight
+      );
+
+      const pointContainer = iq.utils.PointContainer.fromImageData(imageData);
+      const palette = iq.buildPaletteSync([pointContainer], { colors: 4 });
+      const points = palette._pointArray;
+
+      document.getElementById("image-link").href = url;
+      for (let i = 0; i < 4; i++) {
+        document.getElementById(
+          "color-" + i
+        ).style.backgroundColor = pointToRGB(points[i]);
+      }
+    };
+
+    image.src = url;
+  });
